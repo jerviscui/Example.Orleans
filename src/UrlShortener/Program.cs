b177e1acf3efbe3c96cf3598e3964014d7d109dd
jerviscui <cuijervis@126.com>
2024-04-09 18:34:10 +0800

    Update

--- src/UrlShortener/Program.cs
+++ src/UrlShortener/Program.cs
@@ -1,3 +1,5 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Orleans.Configuration;
 using StackExchange.Redis;
 using UrlShortener;
@@ -11,10 +13,11 @@ builder.Host.UseOrleans(siloBuilder =>
     siloBuilder.AddRedisGrainStorage(UrlShortenerGrain.Storage,
         options => { options.ConfigurationOptions = redisConfig; });
 
-    siloBuilder.UseDashboard(options =>
-    {
-        //options.CounterUpdateIntervalMs = 5_000;
-    });
+    //siloBuilder.UseDashboard(options =>
+    //{
+    //    options.CounterUpdateIntervalMs = 5_000;
+    //    options.HistoryLength = 200;
+    //});
 
     siloBuilder.UseLocalhostClustering();
 
@@ -27,9 +30,19 @@ builder.Host.UseOrleans(siloBuilder =>
 
     siloBuilder.AddRedisGrainDirectory(UrlGrain.DistributedDirectory,
         options => { options.ConfigurationOptions = redisConfig; });
+
+    siloBuilder.AddActivityPropagation();
 });
 
-builder.Services.AddOpenTelemetry().WithTracing(providerBuilder => { });
+builder.Services.AddOpenTelemetry().WithTracing(providerBuilder =>
+{
+    providerBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("UrlShortener", "1.0.0"));
+
+    providerBuilder.AddAspNetCoreInstrumentation();
+    providerBuilder.AddSource("Microsoft.Orleans");
+
+    providerBuilder.AddZipkinExporter(options => { options.Endpoint = new Uri("http://localhost:9411/api/v2/spans"); });
+});
 
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
