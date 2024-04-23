317aa839646d87083ff88b09b9502b67d4dbfc7a
jerviscui <cuijervis@126.com>
2024-04-23 14:42:01 +0800

    Delete 2

--- src/road-to-orleans/2/run-client-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-RetrieveIp(){
-  ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'
-}
-
-ADVERTISEDIP=`RetrieveIp`
-
-docker build -t silo-client -f ./ops/Dockerfile-client . &&
-  docker run -it -e ADVERTISEDIP=$ADVERTISEDIP --rm silo-client
--- src/road-to-orleans/2/run-silo-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-RetrieveIp(){
-  ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'
-}
-
-ADVERTISEDIP=`RetrieveIp`
-
-docker build -t silo-host-cluster -f ./ops/Dockerfile-host . &&
-  docker run -it -e ADVERTISEDIP=$ADVERTISEDIP -p 8080:8080 -p 3000:3000 --rm silo-host-cluster
